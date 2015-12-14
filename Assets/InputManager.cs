f6d112115d431c9f35e3f3eec0c3fc3d96a73231
Takahiro Poly Horikawa <horikawa.takahiro@gmail.com>
2015-12-14 16:23:29 +0900

    Add 'fu-fu' function

---
+++ Assets/InputManager.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class InputManager : MonoBehaviour {
+
+	public WindZone wind;
+
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKey (KeyCode.F)) {
+			wind.windMain = 20;
+		} else {
+			wind.windMain = 0;
+		}
+	}
+}
